9817156f8f25599626206b0d1827c3df5df24041
Katelyn Mei <katelynmei00@gmail.com>
2025-10-01 12:50:05 -0700

    update name

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ function draw() {
   rect(width/4, shapeHeight, width/2);
   textSize(20);
   fill("black");
-  text('Hi! My name is Katelyn', currentWidth/2, shapeHeight/2);
+  text('Hi! My name is Katelyn Mei', currentWidth/2, shapeHeight/2);
   
 
 
