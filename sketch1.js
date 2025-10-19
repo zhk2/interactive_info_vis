f8d7dbfae3b0086a35bf1508c10bd2840ad408cf
belencsf <bcsaldias@uc.cl>
2025-10-19 14:54:10 -0700

    Applied linter

--- sketch1.js
+++ sketch1.js
@@ -2,48 +2,48 @@
 let shapeHeight;
 
 let designWidth = 400;
-let designHeight= 400;
-let horizon ;
+let designHeight = 400;
+let horizon;
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  horizon =height/2;
+  createCanvas(windowWidth, windowHeight);
+  horizon = height / 2;
 }
 
 function draw() {
 
-  //shape follows y-coordinate of mouse
-  shapeHeight = mouseY;
+  //shape follows y-coordinate of mouse
+  shapeHeight = mouseY;
   currentWidth = mouseX;
 
-  //light blue background if the shape is above horizon
+  //light blue background if the shape is above horizon
 
   //with if-else statement
   if (shapeHeight < horizon) {
-    background("lightblue"); // blue if above horizon
-    
+    background("lightblue"); // blue if above horizon
+
   } else {
-    background("grey"); // grey if below horizon
+    background("grey"); // grey if below horizon
   }
 
-  //sun
-  fill("white");
-  
-  rect(width/4, shapeHeight, width/2);
+  //sun
+  fill("white");
+
+  rect(width / 4, shapeHeight, width / 2);
   textSize(20);
   fill("black");
-  text('Hi! My name is Katelyn Mei', currentWidth/2, shapeHeight/2);
-  
+  text('Hi! My name is Katelyn Mei', currentWidth / 2, shapeHeight / 2);
 
 
-  // draw line for horizon
-  stroke('lavender');
-  line(0,horizon,width,horizon);
 
-  //grass
+  // draw line for horizon
+  stroke('lavender');
+  line(0, horizon, width, horizon);
 
-  fill("lavender");
+  //grass
 
-  rect(0, horizon, width, height);
+  fill("lavender");
+
+  rect(0, horizon, width, height);
 
 }
 
