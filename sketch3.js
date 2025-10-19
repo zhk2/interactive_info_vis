98467420507073edb89d703ddda45594b38d2aa4
belencsf <bcsaldias@uc.cl>
2025-10-19 15:08:52 -0700

    Refactored code

--- sketch3.js
+++ sketch3.js
@@ -1,14 +1,14 @@
 // simple instance-mode sketch for tab 3
-registerSketch('sk3', function(p) {
-  p.setup = function() {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-  };
-  p.draw = function() {
-    p.background(240, 200, 200);
-    p.fill(180, 60, 60);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('Sketch 3', p.width / 2, p.height / 2);
-  };
-  p.windowResized = function() { p.resizeCanvas(p.windowWidth, p.windowHeight); };
-});
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+function draw() {
+  background(220);
+  fill(100, 150, 240);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text('Sketch 3', width / 2, height / 2);
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
