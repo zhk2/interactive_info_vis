95c3b0054f4f9d60445b8f68799c79da7b5fc378
belencsf <bcsaldias@uc.cl>
2025-10-20 12:09:32 -0700

    Added hour viz for sketch13

--- sketches/sketch13.js
+++ sketches/sketch13.js
@@ -1,7 +1,7 @@
 // Example 9
 registerSketch('sk13', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(600, 600);
   };
 
   p.draw = function () {
@@ -11,13 +11,33 @@ registerSketch('sk13', function (p) {
     const h = p.hour();
     const m = p.minute();
     const s = p.second();
-    const label = p.nf(h, 2) + ':' + p.nf(m, 2) + ':' + p.nf(s, 2);
+    // const label = p.nf(h, 2) + ':' + p.nf(m, 2) + ':' + p.nf(s, 2);
+
+    //Line Width
+    var begin = p.width / 20;
+    var end = (p.width / 20) * 19;
+
+    // Hour Line
+    var hX = begin + (p.hour() / 23 * (end - begin));
+    var hY = p.height / 4;
 
     p.noStroke();
-    p.fill(20);
-    p.textAlign(p.LEFT, p.TOP);  // change to RIGHT/BOTTOM for other corners
+    p.fill(1);
     p.textSize(50);
-    p.text(label, 12, 10);   // top-left corner
+    p.text(p.nf(h, 2), hX - 30, hY - 40);
+
+    p.strokeWeight(20)
+    p.stroke('#413a49ff');
+    p.line(end, hY, begin, hY);
+
+    p.stroke('#bf9fe1ff');
+    p.ellipse(hX, hY, 30, 30);
+
+    p.strokeWeight(10)
+    p.fill('white');
+    p.ellipse(hX, hY, 20, 20);
+
+
 
   }
 
